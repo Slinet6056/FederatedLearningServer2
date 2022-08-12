f63d9bdc6334305f3658915a0b8a304de31c98c7
Slinet6056 <slinet6056@foxmail.com>
2022-08-12 10:34:53 +0800

    自动创建资源文件夹

--- src/main/kotlin/com/slinet/flserver/FlserverApplication.kt
+++ src/main/kotlin/com/slinet/flserver/FlserverApplication.kt
@@ -2,12 +2,25 @@ package com.slinet.flserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
 
 @SpringBootApplication
 class FlserverApplication
 
 fun main(args: Array<String>) {
     runApplication<FlserverApplication>(*args)
+
+    val path = File("res/model")
+    if (path.isDirectory) {
+        Utils.log("Resource directory already exists")
+    } else {
+        if (path.mkdirs()) {
+            Utils.log("Resource directory created successfully")
+        } else {
+            Utils.log("Resource directory creation failed")
+        }
+    }
+
     val socketServer = SocketServer()
     socketServer.startServer(12345)
     ModelAggregation.createModel()
